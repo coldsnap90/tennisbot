67c4f25fa512a110a1c7131331b3e21bf7a10581
colton farbatuk <cfarbatuk@gmail.com>
2024-04-19 19:10:18 -0700

    2024-04-19

--- OpenCV_test.py
+++ OpenCV_test.py
@@ -277,7 +277,7 @@ def book_c(day,court_section ,p_court,p_time,hr,ball,courts,counter):
                     while driver.find_element(By.ID,'servertime').text != '7:59:59 pm':
                             pass
                     
-                time.sleep(0.95)
+                time.sleep(0.925)
                 book.click()
                 print('Program clock ~6.5 seconds faster then tennis server clock, subtract that time from this...',datetime.datetime.now().time())
 
@@ -290,7 +290,7 @@ def book_c(day,court_section ,p_court,p_time,hr,ball,courts,counter):
                     while driver.find_element(By.ID,'servertime').text != '9:59:59 am':
                             pass
                     
-                time.sleep(0.95)
+                time.sleep(0.925)
                 book.click()
                 print('Program clock ~6.5 seconds faster then tennis server clock, subtract that time from this...',datetime.datetime.now().time())
   
